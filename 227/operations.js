150493579f70048050a7e1d087160ad6752ea441
hwafrome <kimkh6212@gmail.com>
2023-02-18 23:39:46 +0900

    섹션11끝

---
+++ 227/operations.js
@@ -0,0 +1,18 @@
+console.log(10+4);
+console.log(10-4);
+console.log(10*4);
+console.log(10/4);
+console.log(10%4);
+console.log(10%3);
+
+console.log((10+3-5)*10);
+
+console.log('Max'+' '+'Schwarzmuller');
+console.log('Max'-'ax'); //Not a Number
+
+let userName='Max';
+console.log(userName.length);
+console.log(userName.toUpperCase());
+
+let hobbies=['Sports','cooking'];
+console.log(hobbies.length);
