1c9a85e1e9035f3556821468e9a865f984afa6b8
hwafrome <kimkh6212@gmail.com>
2023-02-20 16:29:40 +0900

    2/20
    처음에 textContent가 적용이 안됐다

--- 257/app.js
+++ 257/app.js
@@ -4,3 +4,6 @@
 //document.body.children[1].children[0].href='https://www.google.com/'
 let anchorElement=document.getElementById('external-link');
 anchorElement.href='https://google.com';
+
+anchorElement=document.querySelector('p a');
+anchorElement.href='https://naver.com';
