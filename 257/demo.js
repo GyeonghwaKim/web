a0ab93d98cfc16654001eccc8f6ffe849bed7675
hwafrome <kimkh6212@gmail.com>
2023-02-26 23:04:13 +0900

    2/26

--- 257/demo.js
+++ 257/demo.js
@@ -13,5 +13,21 @@ function updateReamainingCharacters(event){
 
     remainingCharsElement.textContent=remainingCharacters;
 
+    if (remainingCharacters===0){
+        remainingCharsElement.classList.add('error');
+        productNameInptElement.classList.add('error');
+
+    }else if (remainingCharacters<=10){
+        remainingCharsElement.classList.add('warning');
+        productNameInptElement.classList.add('warning');
+        remainingCharsElement.classList.remove('error');
+        productNameInptElement.classList.remove('error');
+
+    }else {
+        remainingCharsElement.classList.remove('warning');
+        productNameInptElement.classList.remove('warning');
+
+    }
+
 }
 productNameInptElement.addEventListener('input',updateReamainingCharacters);
