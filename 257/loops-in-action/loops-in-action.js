5070d2869dd9befaf287f57354709b947e11bc14
hwafrome <kimkh6212@gmail.com>
2023-02-28 21:29:39 +0900

    2/28

---
+++ 257/loops-in-action/loops-in-action.js
@@ -0,0 +1,50 @@
+const calculatesSumButonElement=document.querySelector('#calculator button');
+function calculateSum(){
+    const userNumberInputElement=document.getElementById('user-number');
+    const eneteredNumber=userNumberInputElement.value;
+    let sumUpToNumber=0;
+
+    for(let i=0;i<=eneteredNumber;i++){
+        sumUpToNumber=sumUpToNumber+i;
+
+    }
+    const outputResultElement=document.getElementById('calculated-sum')
+
+    outputResultElement.textContent=sumUpToNumber;
+    outputResultElement.style.display='block';
+}
+
+calculatesSumButonElement.addEventListener('click',calculateSum);
+
+//highlight for - of
+
+const highlightLinkesButtonElement=document.querySelector('#highlight-links button');
+function highlightLinks(){
+    const anchorElements=document.querySelectorAll('#highlight-links a');
+
+    for (const anchorElement of anchorElements){
+        anchorElement.classList.add('highlight');
+    }
+}
+highlightLinkesButtonElement.addEventListener('click',highlightLinks);
+
+//Display user data
+const dummyUserData={
+    firstName:'Max',
+    lastName:'Schwarzmuller',
+    age:32
+};
+
+const displayUserDataButtonElement=document.querySelector('#user-data button');
+
+function displayUserData(){
+    const outputDataElement=document.getElementById('output-user-data');
+    outputDataElement.innerHTML='';
+    for(const key in dummyUserData){
+        const newUserDataListItemElement=document.createElement('li');
+        const outputText=key.toUpperCase()+':'+dummyUserData[key];
+        newUserDataListItemElement.textContent=outputText;
+        outputDataElement.append(newUserDataListItemElement);
+}
+}
+displayUserDataButtonElement.addEventListener('click',displayUserData);
