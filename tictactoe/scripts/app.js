985889311dc228394a187d802938d8fe7b019928
hwafrome <kimkh6212@gmail.com>
2023-03-05 02:14:16 +0900

    3/5

---
+++ tictactoe/scripts/app.js
@@ -0,0 +1,5 @@
+const editPlayer1BtenElement=document.getElementById('edit-player-1-btn');
+const editPlayer2BtenElement=document.getElementById('edit-player-2-btn');
+
+editPlayer1BtenElement.addEventListener('click',openPlayerConfig);
+editPlayer2BtenElement.addEventListener('click',openPlayerConfig);
