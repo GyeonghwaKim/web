1ff57fe4ad45a5af746a01cd873deb529e33a7e5
hwafrome <kimkh6212@gmail.com>
2023-03-05 03:08:13 +0900

    3/5

--- tictactoe/scripts/app.js
+++ tictactoe/scripts/app.js
@@ -1,5 +1,11 @@
-const editPlayer1BtenElement=document.getElementById('edit-player-1-btn');
-const editPlayer2BtenElement=document.getElementById('edit-player-2-btn');
+const playerConfigOverlayElement=document.getElementById('config-overlay');
+const backdropElement=document.getElementById('backdrop');
+const cancelConfigBtnElement=document.getElementById('cancel-config-btn');
+const editPlayer1BtnElement=document.getElementById('edit-player-1-btn');
+const editPlayer2BtnElement=document.getElementById('edit-player-2-btn');
 
-editPlayer1BtenElement.addEventListener('click',openPlayerConfig);
-editPlayer2BtenElement.addEventListener('click',openPlayerConfig);
+editPlayer1BtnElement.addEventListener('click',openPlayerConfig);
+editPlayer2BtnElement.addEventListener('click',openPlayerConfig);
+
+cancelConfigBtnElement.addEventListener('click',closePlayerConfig);
+backdropElement.addEventListener('click',closePlayerConfig);
--- tictactoe/scripts/config.js
+++ tictactoe/scripts/config.js
@@ -1,3 +1,9 @@
 function openPlayerConfig(){
-    
+    playerConfigOverlayElement.style.display='block'; //block 보이기 none 안보이기
+    backdropElement.style.display='block';
+}
+
+function closePlayerConfig(){
+    playerConfigOverlayElement.style.display='none'; //block 보이기 none 안보이기
+    backdropElement.style.display='none';
 }
