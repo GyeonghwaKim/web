355de1f1e3f07297834c1c588bd7404ae2edadd2
hwafrome <kimkh6212@gmail.com>
2023-03-06 01:51:42 +0900

    3/6

--- tictactoe/scripts/app.js
+++ tictactoe/scripts/app.js
@@ -4,8 +4,14 @@ const cancelConfigBtnElement=document.getElementById('cancel-config-btn');
 const editPlayer1BtnElement=document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement=document.getElementById('edit-player-2-btn');
 
+const formElement=document.querySelector('form');
+const errorsOutputElement=document.getElementById('config-errors')
+
 editPlayer1BtnElement.addEventListener('click',openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click',openPlayerConfig);
 
 cancelConfigBtnElement.addEventListener('click',closePlayerConfig);
 backdropElement.addEventListener('click',closePlayerConfig);
+
+
+formElement.addEventListener('submit',savePlayerConfig);
